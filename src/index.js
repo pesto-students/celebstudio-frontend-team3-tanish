d3e20d757abe9a2bae155c45f40e804fd3a42656
Jitender Prasad <teejzit@gmail.com>
2022-09-07 20:38:49 +0530

    added localstorage

--- src/index.js
+++ src/index.js
@@ -4,15 +4,18 @@ import './index.css';
 import App from './app';
 import { Provider } from 'react-redux';
 import store from './app/store';
+import { persistStore } from 'redux-persist';
+import { PersistGate } from 'redux-persist/integration/react';
 
-
-
+let persistor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
        <Provider store={store}>
+       <PersistGate persistor={persistor}>
                 <App />
-       </Provider>
+                </PersistGate>
+        </Provider>
        
 );
 
