602fe7c1e0d89db047c9dee6b80bc889b7229b48
Jitender Prasad <teejzit@gmail.com>
2022-09-10 17:47:03 +0530

    clearing the master branch

--- src/businessModules/businessModule/bnotification.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Bnotification = () => {
-  return (
-    <div>Inotification</div>
-  )
-}
-
-export default Bnotification;
--- src/influencerModule/influencerModules/inotification.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-const Inotification = () => {
-  return (
-    <div>Inotification</div>
-  )
-}
-
-export default Inotification;
